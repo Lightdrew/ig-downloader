979e8b0459e2cd2d783f5e060d5b51314b3f0b70
Lightdrew <light02h18@gmail.com>
2025-05-16 14:47:02 +0200

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-
 async fn fetch_reel(url: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    
     let mut video_downloaded = false;
 
     let response = reqwest::Client::new()
@@ -20,7 +18,7 @@ async fn fetch_reel(url: &str) -> Result<(), Box<dyn std::error::Error + Send + 
         .header("Priority", "u=0, i")
         .send()
         .await?;
-    
+
     if response.status().is_success() {
         println!("Response: {}", response.status());
 
@@ -31,12 +29,11 @@ async fn fetch_reel(url: &str) -> Result<(), Box<dyn std::error::Error + Send + 
             if let Some(mut scripts) = document.query_selector("script[type=\"application/json\"]")
             {
                 loop {
-                    
                     let script = match scripts.next() {
                         Some(script) => script,
                         None => break,
                     };
-                    
+
                     let script_text = match script.get(parser) {
                         Some(script_text) => script_text.inner_html(parser),
                         None => continue,
@@ -49,7 +46,8 @@ async fn fetch_reel(url: &str) -> Result<(), Box<dyn std::error::Error + Send + 
 
                     // Commence Json navigation hell
 
-                    let video = json_script.get("require")
+                    let video = json_script
+                        .get("require")
                         .and_then(|x| x.get(0))
                         .and_then(|x| x.get(3))
                         .and_then(|x| x.get(0))
@@ -63,96 +61,106 @@ async fn fetch_reel(url: &str) -> Result<(), Box<dyn std::error::Error + Send + 
                         .and_then(|x| x.get("data"));
 
                     let video = match video {
-                        Some(video) => 
-                        {
-                            video.get("xdt_api__v1__media__shortcode__web_info")
+                        Some(video) => video
+                            .get("xdt_api__v1__media__shortcode__web_info")
                             .or_else(|| video.get("xdt_api__v1__clips__home__connection_v2"))
-                            .or_else(|| video.get("xdt_api__v1__clips__home__no__login__connection_v2"))
-                        },
-                        None => continue,
-                    };
-                    
-                    let video = match video {
-                        Some(video) => 
-                        {
-                            video
-                            .get("edges")
-                            .or_else(|| video.get("items"))
-                        },
+                            .or_else(|| {
+                                video.get("xdt_api__v1__clips__home__no__login__connection_v2")
+                            }),
                         None => continue,
                     };
 
                     let video = match video {
-                        Some(video) => video.get(0),
+                        Some(video) => video
+                            .get("edges")
+                            .or_else(|| video.get("items"))
+                            .and_then(|x| x.get(0)),
                         None => continue,
                     };
 
                     match video {
                         Some(video) => {
-
                             //Attempt downloading the video
 
                             let code = video["code"].as_str().unwrap_or("unknown");
                             let author = video["user"]["username"].as_str().unwrap_or("unknown");
-                            
+
                             println!("Code: {}", code);
                             println!("Author: {}", author);
-                            
-                            let url = video["video_versions"][0]["url"].to_string().replace("\"", "");
+
+                            let url = video["video_versions"][0]["url"]
+                                .to_string()
+                                .replace("\"", "");
 
                             println!("Downloading video...");
-                            
+
                             let video_response = reqwest::get(&url).await?;
 
-                            if video_response.status().is_success() 
-                            {
+                            if video_response.status().is_success() {
                                 let video_bytes = video_response.bytes().await?;
 
-                                tokio::fs::create_dir_all("download").await?;
+                                if !tokio::fs::read_dir("download").await.is_ok() 
+                                {
+                                    match tokio::fs::create_dir("download").await {
+                                        Err(e) => {
+                                            return Err(Box::new(std::io::Error::new(
+                                                std::io::ErrorKind::Other,
+                                                format!(
+                                                    "{}",
+                                                    e
+                                                ),
+                                            )))
+                                        },
+                                        _ => {}
+                                    };
+                                }
 
-                                let mut file = tokio::fs::File::create(format!("download/{}-{}.mp4",author,code)).await?;
-                                
+                                let mut file = tokio::fs::File::create(format!(
+                                    "download/{}-{}.mp4",
+                                    author, code
+                                ))
+                                .await?;
+
                                 tokio::io::copy(&mut video_bytes.as_ref(), &mut file).await?;
-                                
-                                println!("Video downloaded successfully as {}-{}.mp4",author,code);
+
+                                println!(
+                                    "Video downloaded successfully as {}-{}.mp4",
+                                    author, code
+                                );
 
                                 video_downloaded = true;
-                            } else 
-                            {
+                            } else {
                                 return Err(Box::new(std::io::Error::new(
                                     std::io::ErrorKind::Other,
-                                    format!("Failed to download video: Request failed ({})", video_response.status()),
+                                    format!(
+                                        "Failed to download video: Request failed ({})",
+                                        video_response.status()
+                                    ),
                                 )));
                             }
                         }
-                        None => {
-                            continue
-                        },
+                        None => continue,
                     }
                 }
-            }
-            else {
-
+            } else {
                 return Err(Box::new(std::io::Error::new(
                     std::io::ErrorKind::Other,
                     "Failed to fetch reel: No data scripts found",
                 )));
             }
-
-
         } else {
             return Err(Box::new(std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Failed to fetch reel: Failed to parse response",
             )));
         }
-
-
-    }
-    else {
+    } else {
         return Err(Box::new(std::io::Error::new(
             std::io::ErrorKind::Other,
-            format!("Failed to fetch reel: Request failed ({})", response.status()),
+            format!(
+                "Failed to fetch reel: Request failed ({})",
+                response.status()
+            ),
         )));
     }
 
@@ -180,14 +188,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut result = fetch_reel(url).await;
 
     while result.is_err() && retries > 1 {
-        println!("Error: {:?}", result.err());
+        if let Err(e) = &result {
+            println!("{}", e);
+        };
         println!("Retrying...");
         tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
 
         result = fetch_reel(url).await;
         retries -= 1;
     }
-
 
     Ok(())
 }
